a58d79ef5dbe51ff6bdb08d71d1e0c9dd79ead2b
Okomba Mugoma <67360643+mugoma@users.noreply.github.com>
2023-04-17 03:00:31 -0700

    Remove secret key

--- minerva_event_organizer/settings.py
+++ minerva_event_organizer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_s_i!^ti1w47+lgp@x--5t$=v^0p4kb-zbpr4un4s6or^*eh93'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-_s_i!^ti1w47+lgp@x--5t$=v^0p4kb-zbpr4un4s6or^*eh93')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
